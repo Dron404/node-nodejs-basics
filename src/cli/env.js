f664d5744892ba471e12f75963e171fb12620354
Andrei Mashedo <ahdma@mail.ru>
2024-01-24 21:31:00 +0000

    feath: complete cli

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
+import { env } from "node:process";
+
 const parseEnv = () => {
-    // Write your code here 
+  const result = [];
+  for (const key in env) {
+    if (key.match(/^RSS_/)) {
+      result.push(`${key}=${env[key]}`);
+    }
+  }
+  console.log(result.join("; "));
 };
 
 parseEnv();
