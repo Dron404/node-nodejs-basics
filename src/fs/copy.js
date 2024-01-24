d41452d365542840de56a8f72930e97ccdc68ae5
Andrei Mashedo <ahdma@mail.ru>
2024-01-24 18:21:49 +0000

    feath: complete fs

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import fs from "fs/promises";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir("./src/fs/files");
+
+    await fs.mkdir("./src/fs/files_copy");
+    for (const file of files) {
+      await fs.copyFile(
+        `./src/fs/files/${file}`,
+        `./src/fs/files_copy/${file}`
+      );
+    }
+  } catch (e) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,16 @@
+import fs from "fs/promises";
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const files = await fs.readdir("./src/fs/files/");
+
+    if (files.includes("fresh.txt")) {
+      throw new Error("FS operation failed: File already exists.");
+    }
+    await fs.writeFile("./src/fs/files/fresh.txt", "I am fresh and young");
+  } catch (e) {
+    throw new Error(e.message);
+  }
 };
 
 await create();
