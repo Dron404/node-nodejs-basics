71cc34d0ea99476b34163ab1f0b7332412dce09e
Andrei Mashedo <ahdma@mail.ru>
2024-01-27 23:22:56 +0300

    feth: compete zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { createGunzip } from "zlib";
+
 const decompress = async () => {
-    // Write your code here 
+  const filename = fileURLToPath(import.meta.url);
+  const readStream = createReadStream(
+    path.join(dirname(filename), "files",  "archive.gz")
+  );
+  const writeStream = createWriteStream(
+    path.join(dirname(filename), "files", "fileToCompress.txt")
+  );
+  const guZlib = createGunzip();
+  readStream.pipe(guZlib).pipe(writeStream);
 };
 
 await decompress();
