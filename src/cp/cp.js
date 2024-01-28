b153c83d2ac6bf47989864aaedca99e882673f2c
Andrei Mashedo <ahdma@mail.ru>
2024-01-28 16:02:12 +0300

    feath: complete cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const filename = fileURLToPath(import.meta.url);
+
+  const children = spawn("node", [
+    path.join(dirname(filename), "files", "script.js"),
+    ...args,
+  ]);
+  children.stdout.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
+  process.stdin.on("data", (data) => {
+    children.stdin.write(data.toString());
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
